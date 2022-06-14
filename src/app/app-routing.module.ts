dbf6f7761285bdcea9de53cb8159d8bd44c8027f
Marlon Andrei <marlon-master@hotmail.com>
2022-06-14 17:48:04 -0300

    lazy loading gg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // lazy loading
+  {
+    path: '',
+    loadChildren: () =>
+      import('./core/components/auth/auth.module').then((m) => m.AuthModule),
+  },
+  // lazy loading
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./components/admin/admin.module').then((m) => m.AdminModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
